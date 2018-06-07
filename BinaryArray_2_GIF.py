96f111e6c091fdf031dab1fb6f22534400eee337
Kieran Fitzmaurice <kpf10@pitt.edu>
2018-06-07 14:15:55 -0400

    Python script to create GIFs from fortran output files

--- BinaryArray_2_GIF.py
+++ BinaryArray_2_GIF.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PDE_Modules import ReadArray_FortranBinary, MakeGIF_2D
 
-inputfile = "/Users/kieranfitzmaurice/Documents/MATLAB/Spinodal.dat"
-outputfile = "/Users/kieranfitzmaurice/Desktop/Spinodal.gif"
+inputfile = "/Users/kieranfitzmaurice/Documents/REU_2018/PDE_data/temp_file.dat"
+outputfile = "/Users/kieranfitzmaurice/Documents/REU_2018/PDE_data/temp_file.gif"
 
 A = ReadArray_FortranBinary(inputfile,3)
 MakeGIF_2D(outputfile,A)
