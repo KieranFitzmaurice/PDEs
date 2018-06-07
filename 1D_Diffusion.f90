daf82d4e1f360f0a2935b6a2db326a148fa786a3
Kieran Fitzmaurice <kpf10@pitt.edu>
2018-06-07 14:14:36 -0400

    1-dimensional diffusion equation

--- 1D_Diffusion.f90
+++ 1D_Diffusion.f90
@@ -1,59 +1,59 @@
 PROGRAM Diffusion
-! This program uses an FTCS scheme to solve the 1D Diffusion Equation
+  ! This program uses an FTCS scheme to solve the 1D Diffusion Equation
 
-USE output_arrays
-IMPLICIT NONE
+  USE output_arrays
+  IMPLICIT NONE
 
-INTERFACE
-   SUBROUTINE u_analytic(x,x_0,t,t_0,u)
-     REAL(KIND = 8), INTENT(IN) :: x_0, t, t_0
-     REAL(KIND = 8), DIMENSION(:), INTENT(IN) :: x
-     REAL(KIND = 8), DIMENSION(:), INTENT(OUT) :: u
-   END SUBROUTINE u_analytic
-END INTERFACE
+  INTERFACE
+     SUBROUTINE u_analytic(x,x_0,t,t_0,u)
+       REAL(KIND = 8), INTENT(IN) :: x_0, t, t_0
+       REAL(KIND = 8), DIMENSION(:), INTENT(IN) :: x
+       REAL(KIND = 8), DIMENSION(:), INTENT(OUT) :: u
+     END SUBROUTINE u_analytic
+  END INTERFACE
 
-INTEGER :: i, n, t_steps
-REAL(KIND = 8) :: k, l, dx, dt, max_dt,t_0,t_f,x_0
-REAL(KIND = 8), ALLOCATABLE :: x(:), u(:), concentration(:,:)
-CHARACTER(LEN = 100) :: filename
+  INTEGER :: i, n, t_steps
+  REAL(KIND = 8) :: k, l, dx, dt, max_dt,t_0,t_f,x_0
+  REAL(KIND = 8), ALLOCATABLE :: x(:), u(:), concentration(:,:)
+  CHARACTER(LEN = 100) :: filename
 
-filename = '/Users/kieranfitzmaurice/Documents/MATLAB/diffusion.dat'
+  filename = '/Users/kieranfitzmaurice/Documents/MATLAB/diffusion.dat'
 
-k = 1 ! Diffusion coefficient
-l = 20 ! Length of rod
-x_0 = l/2 ! Peak of concentration at beginning (used for analytical solution)
-n = 200 ! Number of points in rod
+  k = 1 ! Diffusion coefficient
+  l = 20 ! Length of rod
+  x_0 = l/2 ! Peak of concentration at beginning (used for analytical solution)
+  n = 200 ! Number of points in rod
 
-dx = l/(n-1)
-max_dt = (0.5*dx**2)/k ! Von Neumman Stability
-dt = 0.4*max_dt
+  dx = l/(n-1)
+  max_dt = (0.5*dx**2)/k ! Von Neumman Stability
+  dt = 0.4*max_dt
 
-t_0 = 0.1
-t_f = 10
+  t_0 = 0.1
+  t_f = 10
 
-t_steps = FLOOR((t_f - t_0)/dt)
+  t_steps = FLOOR((t_f - t_0)/dt)
 
-! Array concentration stores how u changes in time
-ALLOCATE(x(1:n))
-ALLOCATE(u(1:n))
-ALLOCATE(concentration(1:n,1:t_steps))
+  ! Array concentration stores how u changes in time
+  ALLOCATE(x(1:n))
+  ALLOCATE(u(1:n))
+  ALLOCATE(concentration(1:n,1:t_steps))
 
-! Store x positions
-DO i = 1,n
-  x(i) = (i-1)*dx
-ENDDO
+  ! Store x positions
+  DO i = 1,n
+     x(i) = (i-1)*dx
+  ENDDO
 
-! Get initial concentration profile from analytical solution
-CALL u_analytic(x,x_0,t_0,t_0,u)
+  ! Get initial concentration profile from analytical solution
+  CALL u_analytic(x,x_0,t_0,t_0,u)
 
-! Use FTCS method to numerically solve diffusion
-DO i = 1,t_steps
-  u = u + dt*(CSHIFT(u,SHIFT = -1) - 2*u + CSHIFT(u,SHIFT = 1))/dx**2
-  concentration(:,i) = u
-ENDDO
+  ! Use FTCS method to numerically solve diffusion
+  DO i = 1,t_steps
+     u = u + dt*(CSHIFT(u,SHIFT = -1) - 2*u + CSHIFT(u,SHIFT = 1))/dx**2
+     concentration(:,i) = u
+  ENDDO
 
-! Write concentration profiles to output file
-CALL output2D_binary(filename,concentration,n,t_steps)
+  ! Write concentration profiles to output file
+  CALL output2D_binary(filename,concentration,n,t_steps)
 
 END PROGRAM Diffusion
 
