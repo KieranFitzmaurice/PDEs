e3babb37507114c9aa36c1a935cafb843fdfbf38
Kieran Fitzmaurice <kpf10@pitt.edu>
2018-06-09 15:36:07 -0400

    Fortran Array I/O module

--- array_io.f90
+++ array_io.f90
@@ -3,6 +3,76 @@ CONTAINS
 
   !****************************************************************************!
 
+  SUBROUTINE size1D_binary(filename,n1)
+    ! This subroutine reads in the length of a 1D array from a binary file.
+    ! This binary file should follow the same format as those that are written
+    ! by the array output subroutines in this module.
+
+    CHARACTER(LEN = *), INTENT(IN) :: filename
+    INTEGER, INTENT(OUT):: n1
+
+    OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) n1
+    CLOSE(1)
+
+  END SUBROUTINE size1D_binary
+
+  !****************************************************************************!
+
+  SUBROUTINE size2D_binary(filename,n1,n2)
+    ! This subroutine reads in the dimensions of a 2D array from a binary file.
+    ! This binary file should follow the same format as those that are written
+    ! by the array output subroutines in this module.
+
+    CHARACTER(LEN = *), INTENT(IN) :: filename
+    INTEGER, INTENT(OUT):: n1, n2
+
+    OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) n1
+    READ(1) n2
+    CLOSE(1)
+
+  END SUBROUTINE size2D_binary
+
+  !****************************************************************************!
+
+  SUBROUTINE size3D_binary(filename,n1,n2,n3)
+    ! This subroutine reads in the dimensions of a 3D array from a binary file.
+    ! This binary file should follow the same format as those that are written
+    ! by the array output subroutines in this module.
+
+    CHARACTER(LEN = *), INTENT(IN) :: filename
+    INTEGER, INTENT(OUT):: n1, n2, n3
+
+    OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) n1
+    READ(1) n2
+    READ(1) n3
+    CLOSE(1)
+
+  END SUBROUTINE size3D_binary
+
+  !****************************************************************************!
+
+  SUBROUTINE size4D_binary(filename,n1,n2,n3,n4)
+    ! This subroutine reads in the dimensions of a 4D array from a binary file.
+    ! This binary file should follow the same format as those that are written
+    ! by the array output subroutines in this module.
+
+    CHARACTER(LEN = *), INTENT(IN) :: filename
+    INTEGER, INTENT(OUT):: n1, n2, n3, n4
+
+    OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) n1
+    READ(1) n2
+    READ(1) n3
+    READ(1) n4
+    CLOSE(1)
+
+  END SUBROUTINE size4D_binary
+
+  !****************************************************************************!
+
   FUNCTION input1D_binary(filename,n1) RESULT(A)
     ! This subroutine reads in a 1D array from a binary file
     CHARACTER(LEN = *), INTENT(IN) :: filename
@@ -10,6 +80,7 @@ CONTAINS
     REAL(KIND = 8), DIMENSION(n1) :: A
 
     OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) ! Skip entry specifying value of n1
     READ(1) A(1:n1)
     CLOSE(1)
 
@@ -24,7 +95,10 @@ CONTAINS
     REAL(KIND = 8), DIMENSION(n1,n2) :: A
 
     OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) ! Skip entry specifying value of n1
+    READ(1) ! Skip entry specifying value of n2
     READ(1) A(1:n1,1:n2)
+
     CLOSE(1)
 
   END FUNCTION input2D_binary
@@ -38,6 +112,9 @@ CONTAINS
     REAL(KIND = 8), DIMENSION(n1,n2,n3) :: A
 
     OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) ! Skip entry specifying value of n1
+    READ(1) ! Skip entry specifying value of n2
+    READ(1) ! Skip entry specifying value of n3
     READ(1) A(1:n1,1:n2,1:n3)
     CLOSE(1)
 
@@ -52,6 +129,10 @@ CONTAINS
     REAL(KIND = 8), DIMENSION(n1,n2,n3,n4) :: A
 
     OPEN(UNIT=1,FILE=filename,FORM ='UNFORMATTED',STATUS='OLD',ACTION='READ')
+    READ(1) ! Skip entry specifying value of n1
+    READ(1) ! Skip entry specifying value of n2
+    READ(1) ! Skip entry specifying value of n3
+    READ(1) ! Skip entry specifying value of n4
     READ(1) A(1:n1,1:n2,1:n3,1:n4)
     CLOSE(1)
 
